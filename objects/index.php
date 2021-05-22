3b76448020508a4de4749e15b60011b62757aae2
Aleksa Obradovic <obradovic.aleksa@gmail.com>
2021-05-22 21:31:46 +0200

    metode

--- objects/index.php
+++ objects/index.php
@@ -2,17 +2,33 @@
 
 class ShopProduct {
 
+    public $title;
+    public $price;
+
+    function __construct($title, $price) // argumenti - lenovo, 55000 itd
+    {   
+        $this->title = $title; // laptopu ili book dodaj title 
+        $this->price = $price;
+    }
+
+    function showInfo(){
+        echo $this->title." ".$this->price;
+    }
+
 }
 
-$laptop = new ShopProduct();
-$laptop->title = "Lenovo Y799";
+$laptop = new ShopProduct("Lenovo Y799", 55000);
 
-$book = new ShopProduct();
-$book->title = "Semestar 34";
-$book->price = 1300;
+
+$book = new ShopProduct("Semestar 34", 1300);
+
 
 //var_dump($book);
 
-echo "<h1>".$book->title."</h1>";
+//echo "<h1>".$book->title."</h1>";
+
+
+$book->showInfo();
+
 
 ?>
