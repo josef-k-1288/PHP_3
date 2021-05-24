c84ad072fe9522484dff15a853b478511e012a60
Aleksa Obradovic <obradovic.aleksa@gmail.com>
2021-05-24 15:57:31 +0200

    type hinting

--- objects/index.php
+++ objects/index.php
@@ -2,7 +2,7 @@
 
 class ShopProduct {
 
-    public $title;
+    public $title; // public metodi moze da se pristupi izvan klase a private ne moze
     public $price;
 
     function __construct($title, $price) // argumenti - lenovo, 55000 itd
@@ -15,11 +15,13 @@ class ShopProduct {
         echo $this->title." ".$this->price;
     }
 
+    public function changeTitle($new_title){
+        $this->title = $new_title; // funkcija za promenu titla
+    }
+
 }
 
 $laptop = new ShopProduct("Lenovo Y799", 55000);
-
-
 $book = new ShopProduct("Semestar 34", 1300);
 
 
@@ -28,7 +30,30 @@ $book = new ShopProduct("Semestar 34", 1300);
 //echo "<h1>".$book->title."</h1>";
 
 
-$book->showInfo();
+//$book->showInfo();
+
+//$book->title = "Semestar 4"; // promena vrednosti title za book
+//$book->changeTitle("Semestar 4"); // pozivanje funckije za promenu titla
+//$book->showInfo();
+
+class Printer {
+    public function display(ShopProduct $oneShopItem)
+    {
+        echo $oneShopItem->title." ".$oneShopItem->price;
+    }
+}
+
+$hp = new Printer();
+$hp->display($laptop);
+
+
+function saySomething(array $names)
+{
+    var_dump($names);
+}
+
+saySomething(["Danilo", 'Ivko']);
+
 
 
 ?>
