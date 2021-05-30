9c2bed4b684f6acecd7c1ae3a0a881b3c34eadf8
Aleksa Obradovic <obradovic.aleksa@gmail.com>
2021-05-30 19:08:13 +0200

    todos 2

--- todos/bootstrap.php
+++ todos/bootstrap.php
@@ -7,4 +7,9 @@ require 'Connection.php';
 $db = Connection::connect($config['database']); // prosledjivanje iz configa array database
 
 
+require 'Todo.php';
+require 'QueryBuilder.php';
+
+$query = new QueryBuilder($db);
+
 ?>
--- todos/index.php
+++ todos/index.php
@@ -1,6 +1,13 @@
 <?php 
 
 require 'bootstrap.php';
+
+
+
+$todos = $query->selectAll('todos','Todo');
+
+require 'views/index.view.php';
+
 ?>
 
 
